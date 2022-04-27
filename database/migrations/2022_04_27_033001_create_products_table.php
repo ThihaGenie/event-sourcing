4b93842c1df0c72b241b13e727d0b333a69be882
Soe Thiha Naung <103924865+ThihaGenie@users.noreply.github.com>
2022-04-27 10:44:13 +0700

    Database Setup

--- database/migrations/2022_04_27_033001_create_products_table.php
+++ database/migrations/2022_04_27_033001_create_products_table.php
@@ -13,15 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->text("address");
-            $table->string('phone')->unique();
-            $table->rememberToken();
+            $table->string("name");
+            $table->double("unit_price");
+            $table->integer("unit_count");
             $table->timestamps();
         });
     }
@@ -33,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
 };
--- database/migrations/2022_04_27_033015_create_orders_table.php
+++ database/migrations/2022_04_27_033015_create_orders_table.php
@@ -13,15 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->text("address");
-            $table->string('phone')->unique();
-            $table->rememberToken();
+            $table->integer("user_id")->references("id")->on("users");
+            $table->date("order_date");
+            $table->enum("order_status", ["processing", "canceled", "delivering", "delivered", "completed"]);
             $table->timestamps();
         });
     }
@@ -33,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('orders');
     }
 };
